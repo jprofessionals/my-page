fe8a0fccb2c6a61400799e665b566db8346d1721
Marenweidemann <maren@jpro.no>
2022-06-07 16:03:05 +0200

    New version of navbar

--- my-page-app/src/components/navbar/Navbar.js
+++ my-page-app/src/components/navbar/Navbar.js
@@ -1,13 +1,44 @@
 import React from "react";
-import { Link, Router } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-      <li>
-        <Link to="/">Hjem</Link>
-      </li>
-    </div>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      {/* <a className="navbar-brand" href="#">
+        Navbar
+      </a> */}
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">
+              Hjem
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Min profil
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Mine utlegg
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
+
 export default Navbar;
