5a45a30d82165bb1f2bfd2eda824575d16217456
Marenweidemann <maren@jpro.no>
2022-06-10 12:17:03 +0200

    Fix packages and useState and useEffect

--- my-page-app/src/App.jsx
+++ my-page-app/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import LoginHooks from "./components/login/LoginHooks";
 import LogoutHooks from "./components/login/LogoutHooks";
@@ -7,11 +7,25 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "./components/navbar/NavBar";
 
 function App() {
+  const [isLoggedIn, checkLoggedIn] = useState(false);
+  useEffect(() => {
+    if (localStorage.getItem("user_token") != null) {
+      // checkLoggedIn(true);
+      console.log(isLoggedIn);
+    } else {
+      // checkLoggedIn(false);
+      console.log(isLoggedIn);
+    }
+  }, [isLoggedIn]);
+  // console.log(isLoggedIn);
   return (
     <div className="App">
       <NavBar />
-      <LoginHooks />
-      <LogoutHooks />
+      {isLoggedIn ? (
+        <LogoutHooks onclick={() => checkLoggedIn(false)} />
+      ) : (
+        <LoginHooks onclick={() => checkLoggedIn(true)} />
+      )}
       <Home />
     </div>
   );
