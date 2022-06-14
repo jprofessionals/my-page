c7d52a5705bebca94816d51a6c949b8f4b6d8e1b
Marenweidemann <maren@jpro.no>
2022-06-14 11:04:25 +0200

    remove unused import

--- my-page-app/src/App.jsx
+++ my-page-app/src/App.jsx
@@ -1,42 +1,35 @@
-import React, { useEffect, useState, refetch, Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import LoginHooks from "./components/login/LoginHooks";
 import LogoutHooks from "./components/login/LogoutHooks";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "./components/navbar/NavBar";
 
-
 function App() {
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
+  const setAuthentication = () => {
+    setIsAuthenticated(true);
+  };
 
-    const setAuthentication = () => {
-        setIsAuthenticated(true);
-    }
+  const removeAuthentication = () => {
+    setIsAuthenticated(false);
+  };
 
-    const removeAuthentication = () => {
-        setIsAuthenticated(false);
-    }
-
-        if (!isAuthenticated){
-            return (
-                <div>
-                <LoginHooks authentication={setAuthentication} />
-                </div>
-            )
-        }
-        else {
-            return (
-                <div>
-                     <NavBar />
-                    <LogoutHooks authentication={removeAuthentication}  />
-        
-                </div> 
-            );
-            
-
-        }
-
+  if (!isAuthenticated) {
+    return (
+      <div>
+        <LoginHooks authentication={setAuthentication} />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <NavBar />
+        <LogoutHooks authentication={removeAuthentication} />
+      </div>
+    );
+  }
 }
 
 export default App;
