9d18075b970c9155d4cee1c7ec57546c9c5e2e2c
Marenweidemann <maren@jpro.no>
2022-06-15 12:23:32 +0200

    Add user entity and getUsers

--- my-page-api/src/main/kotlin/no/jpro/mypageapi/repository/ArticleRepository.kt
+++ my-page-api/src/main/kotlin/no/jpro/mypageapi/repository/ArticleRepository.kt
@@ -1,8 +1,12 @@
 package no.jpro.mypageapi.repository
 
 import no.jpro.mypageapi.model.Employee
+import no.jpro.mypageapi.model.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface EmployeeRepository : JpaRepository<Employee, Long>
+
+@Repository
+interface UserRepository : JpaRepository<User,String>
