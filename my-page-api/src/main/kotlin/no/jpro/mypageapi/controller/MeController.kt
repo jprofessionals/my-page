b76bbac5019cc1a85d13ecfc9f7216cce810f461
Roger A. Slaaen <roger@jpro.no>
2022-06-20 08:18:24 +0200

    cleanup

--- my-page-api/src/main/kotlin/no/jpro/mypageapi/controller/MeController.kt
+++ my-page-api/src/main/kotlin/no/jpro/mypageapi/controller/MeController.kt
@@ -1,11 +1,12 @@
 package no.jpro.mypageapi.controller
 
 import no.jpro.mypageapi.dto.UserDTO
-import no.jpro.mypageapi.repository.UserRepository
 import no.jpro.mypageapi.service.UserService
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.security.oauth2.jwt.Jwt
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("me")
--- my-page-api/src/main/kotlin/no/jpro/mypageapi/controller/TestController.kt
+++ my-page-api/src/main/kotlin/no/jpro/mypageapi/controller/TestController.kt
@@ -1,19 +1,14 @@
 package no.jpro.mypageapi.controller
 
-import no.jpro.mypageapi.dto.UserDTO
-import no.jpro.mypageapi.repository.UserRepository
-import no.jpro.mypageapi.service.UserService
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController()
 @RequestMapping("test")
-class TestController(private val userRepository: UserRepository, private val userService: UserService) {
+class TestController() {
     @GetMapping("")
     fun sayHelloSecure(): String? {
         return String.format("Hello from the secure API.")
     }
-
-
-
-
 }
