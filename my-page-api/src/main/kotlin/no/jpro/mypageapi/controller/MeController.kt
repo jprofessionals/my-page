71a3d6e85e43a4258a4c435276b32525fbf2e55f
Roger A. Slaaen <roger@jpro.no>
2022-06-22 07:53:06 +0200

    Update swagger for me controller

--- my-page-api/src/main/kotlin/no/jpro/mypageapi/controller/MeController.kt
+++ my-page-api/src/main/kotlin/no/jpro/mypageapi/controller/MeController.kt
@@ -1,5 +1,10 @@
 package no.jpro.mypageapi.controller
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import no.jpro.mypageapi.dto.UserDTO
 import no.jpro.mypageapi.service.UserService
@@ -14,5 +19,11 @@ import org.springframework.web.bind.annotation.RestController
 @SecurityRequirement(name = "Bearer Authentication")
 class MeController(private val userService: UserService) {
     @GetMapping("")
-    fun getCurrentLoggedInUser(@AuthenticationPrincipal jwt: Jwt): UserDTO = userService.getOrCreateUser(jwt)
+    @Operation(summary = "Get data for user identified by the bearer token")
+    @ApiResponse(
+        responseCode = "200",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = UserDTO::class))]
+    )
+    fun getCurrentLoggedInUser(@Parameter(hidden = true) @AuthenticationPrincipal jwt: Jwt): UserDTO =
+        userService.getOrCreateUser(jwt)
 }
