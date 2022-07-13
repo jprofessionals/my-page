8c5dcc25d986884dab6110ac7f4115652121f7aa
Marenweidemann <maren@jpro.no>
2022-07-13 15:46:05 +0200

    get newest api functionality and fix styling

---
+++ my-page-app/src/components/budget/EditBudgetPost.jsx
@@ -0,0 +1,134 @@
+import React, { useState } from "react";
+import ApiService from "../../services/api.service";
+import Moment from "moment";
+import { Card, Button, Spinner } from "react-bootstrap";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRemove, faCheck } from "@fortawesome/free-solid-svg-icons";
+
+const EditBudgetPost = ({
+  toggle,
+  refreshBudgets,
+  budget,
+  post,
+  setIsLoadingPost,
+  isLoadingPost,
+}) => {
+  const [description, setDescription] = useState("");
+  const [amount, setAmount] = useState(0);
+  const [date, setDate] = useState(Moment().format("YYYY-MM-DD"));
+
+  const isValid = () => {
+    return amount > 0 && description && description !== "";
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!isValid()) {
+      toast.error("Noen av verdiene var ikke gyldig, prøv igjen");
+    } else {
+      setIsLoadingPost(true);
+      const budgetPost = {
+        date: date,
+        description: description,
+        amount: amount,
+      };
+      ApiService.editBudgetPost(post.id, budgetPost).then(
+        (response) => {
+          refreshBudgets();
+          toggle();
+          setIsLoadingPost(false);
+          toast.success("Lagret", post.description);
+        },
+        (error) => {
+          setIsLoadingPost(false);
+          toast.error("Fikk ikke oppdatert", post.description, "prøv igjen");
+        }
+      );
+    }
+  };
+
+  const handleDescriptionChange = (e) => {
+    setDescription(e.target.value);
+  };
+
+  const handleAmountChange = (e) => {
+    setAmount(e.target.value);
+  };
+
+  const handleDateChange = (e) => {
+    setDate(e.target.value);
+  };
+
+  return (
+    <Card className="inputCard editPost">
+      <Card.Header className="editPostHeader">
+        <input
+          className="description"
+          type="text"
+          name="description"
+          placeholder={post.description}
+          onChange={handleDescriptionChange}
+          value={description}
+        />
+        <div className="editCardBtns">
+          <Button
+            className="addPostBtn"
+            type="btn submit"
+            style={isValid() ? {} : { display: "none" }}
+            onClick={handleSubmit}
+            title="Lagre Post"
+          >
+            <div style={isLoadingPost ? { display: "none" } : {}}>
+              <FontAwesomeIcon icon={faCheck} />
+            </div>
+            <div style={isLoadingPost ? {} : { display: "none" }}>
+              <Spinner animation="border" size="sm" />
+            </div>
+          </Button>
+          <Button
+            className="canselEditButton"
+            type="btn"
+            title="Avbryt redigering"
+            onClick={toggle}
+          >
+            <div style={isLoadingPost ? { display: "none" } : {}}>
+              <FontAwesomeIcon icon={faRemove} />
+            </div>
+            <div style={isLoadingPost ? {} : { display: "none" }}>
+              <Spinner animation="border" size="sm" />
+            </div>
+          </Button>
+        </div>
+      </Card.Header>
+      <Card.Body>
+        <ul className="addPost">
+          <li>
+            <span className="priceTitle">Pris:</span>
+            <input
+              type="number"
+              name="amount"
+              placeholder={post.amount}
+              onChange={handleAmountChange}
+              value={amount}
+            />
+          </li>
+          <li>
+            <span className="datoTitle">Dato:</span>
+            <input
+              className="inputDate"
+              type="date"
+              name="date"
+              onChange={handleDateChange}
+              value={date}
+              placeholder={post.date}
+            ></input>
+          </li>
+        </ul>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default EditBudgetPost;
--- my-page-app/src/components/budget/Post.jsx
+++ my-page-app/src/components/budget/Post.jsx
@@ -2,12 +2,14 @@ import React, { useState } from "react";
 import { Card, Button, Spinner } from "react-bootstrap";
 import Moment from "moment";
 import DeleteBudgetPostModal from "./DeleteBudgetPostModal";
+import EditBudgetPost from "./EditBudgetPost";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faTrash, faEdit } from "@fortawesome/free-solid-svg-icons";
 
-const Post = ({ refreshBudgets, post, budgetId }) => {
+const Post = ({ refreshBudgets, post, budget }) => {
   const [isLoadingPost, setIsLoadingPost] = useState(false);
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [isEditPostOpen, setIsEditPostOpen] = useState(false);
 
   const postInFuture = () => {
     return post.date > Moment().format("YYYY-MM-DD");
@@ -21,45 +23,87 @@ const Post = ({ refreshBudgets, post, budgetId }) => {
     }
   };
 
+  const togglerEdit = () => {
+    if (isEditPostOpen) {
+      console.log("hei");
+      setIsEditPostOpen(false);
+    } else {
+      console.log("hei");
+      setIsEditPostOpen(true);
+    }
+  };
   return (
-    <Card border={postInFuture() ? "grey" : "dark"}>
-      <div className="postHeader">
-        <Card.Header>
-            <b>{post.description}</b>
-            <Button
-              className="removePostBtn btn"
-              type="btn"
-              title="Slett post"
-              onClick={toggler}
-            >
-              <DeleteBudgetPostModal
-                isDeleteModalOpen={isDeleteModalOpen}
-                toggler={toggler}
-                refreshBudgets={refreshBudgets}
-                post={post}
-                setIsLoadingPost={setIsLoadingPost}
-              />
-              <div title="Logg ut">
-                <div style={isLoadingPost ? { display: "none" } : {}}>
-                  <FontAwesomeIcon icon={faTrash} />
-                </div>
-                <div style={isLoadingPost ? {} : { display: "none" }}>
-                  <Spinner animation="border" size="sm" />
-                </div>
+    <div className="post">
+      <Card
+        border={postInFuture() ? "grey" : "dark"}
+        style={isEditPostOpen ? { display: "none" } : {}}
+      >
+        <div className="postHeaderDiv">
+          <Card.Header className="postHeader">
+            <div className="headerPost">
+              <b>{post.description}</b>
+              <div className="btns">
+                <Button
+                  className="editPostBtn"
+                  type="btn"
+                  title="Rediger post"
+                  onClick={togglerEdit}
+                >
+                  <div title="Rediger Post">
+                    <div style={isLoadingPost ? { display: "none" } : {}}>
+                      <FontAwesomeIcon icon={faEdit} />
+                    </div>
+                    <div style={isLoadingPost ? {} : { display: "none" }}>
+                      <Spinner animation="border" size="sm" />
+                    </div>
+                  </div>
+                </Button>
+                <Button
+                  className="removePostBtn"
+                  type="btn"
+                  title="Slett post"
+                  onClick={toggler}
+                >
+                  <DeleteBudgetPostModal
+                    isDeleteModalOpen={isDeleteModalOpen}
+                    togglerDelete={toggler}
+                    refreshBudgets={refreshBudgets}
+                    post={post}
+                    setIsLoadingPost={setIsLoadingPost}
+                  />
+                  <div title="Logg ut">
+                    <div style={isLoadingPost ? { display: "none" } : {}}>
+                      <FontAwesomeIcon icon={faTrash} />
+                    </div>
+                    <div style={isLoadingPost ? {} : { display: "none" }}>
+                      <Spinner animation="border" size="sm" />
+                    </div>
+                  </div>
+                </Button>
               </div>
-            </Button>
-        </Card.Header>
+            </div>
+          </Card.Header>
+        </div>
+        <Card.Body>
+          <Card.Text>
+            <b>Pris:</b> {post.amount}
+          </Card.Text>
+          <Card.Text>
+            <b>Dato:</b> {post.date}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <div style={isEditPostOpen ? {} : { display: "none" }}>
+        <EditBudgetPost
+          toggle={togglerEdit}
+          refreshBudgets={refreshBudgets}
+          budget={budget}
+          post={post}
+          setIsLoadingPost={setIsLoadingPost}
+          isLoadingPost={isLoadingPost}
+        />
       </div>
-      <Card.Body>
-        <Card.Text>
-          <b>Pris:</b> {post.amount}
-        </Card.Text>
-        <Card.Text>
-          <b>Dato:</b> {post.date}
-        </Card.Text>
-        <div style={!post.locked ? {} : { display: "none" }}></div>
-      </Card.Body>
-    </Card>
+    </div>
   );
 };
 
