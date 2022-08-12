ca0ecd4d51d796da70872223865841cd404bc7b9
Roger A. Slaaen <roger@jpro.no>
2022-08-12 11:29:50 +0200

    Use new google login

--- my-page-app/src/App.jsx
+++ my-page-app/src/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import "./App.scss";
-import LoginHooks from "./components/login/LoginHooks";
 import NavBar from "./components/navbar/NavBar";
 import Home from "./components/home";
 import ApiService from "./services/api.service";
@@ -9,11 +8,63 @@ import Budgets from "./components/budget/Budgets";
 import { Spinner } from "react-bootstrap";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import jPro_logo_transparent from "./components/images/jPro_logo_transparent.svg";
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [user, setUser] = useState(new User());
   const [loadUser, setLoadUser] = useState(false);
+
+  const [scriptLoaded, setScriptLoaded] = useState(false);
+
+  function handleGoogleSignIn(response) {
+    if (response.credential) {
+      localStorage.setItem("user_token", JSON.stringify({ 'id_token': response.credential }));
+    }
+    setIsAuthenticated(true);
+  }
+
+  useEffect(() => {
+    if (scriptLoaded) return undefined;
+
+    const initializeGoogle = () => {
+      if (!window.google || scriptLoaded) return;
+
+      setScriptLoaded(true);
+      window.google.accounts.id.initialize({
+        client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+        auto_select: true,
+        prompt_parent_id: "signInDiv",
+        callback: handleGoogleSignIn,
+      });
+      window.google.accounts.id.prompt((notification) => {
+        if (notification.isNotDisplayed() || notification.isSkippedMoment()) {
+          window.google.accounts.id.renderButton(
+            document.getElementById("signInDiv"),
+            {
+              type: "standard",
+              theme: "outline",
+              size: "medium",
+              text: "continue_with",
+              shape: "rectangular"
+            }
+          );
+        }
+      });
+    };
+
+    const script = document.createElement("script");
+    script.src = "https://accounts.google.com/gsi/client";
+    script.onload = initializeGoogle;
+    script.async = true;
+    script.id = "google-client-script";
+    document.querySelector("body")?.appendChild(script);
+
+    return () => {
+      window.google?.accounts.id.cancel();
+      document.getElementById("google-client-script")?.remove();
+    };
+  }, [scriptLoaded]);
 
   useEffect(() => {
     if (isAuthenticated) {
@@ -46,17 +97,19 @@ function App() {
 
   if (!isAuthenticated) {
     return (
-      <div>
-        <LoginHooks isAuthenticatedCallBack={setIsAuthenticated} />
-        <ToastContainer
+      <div className="vertical-center">
+        <img className="logo" src={jPro_logo_transparent} alt="jPro" />
+        {/* <h2 className="headerText">Velkommen til Min side!</h2> */}
+        <div id="signInDiv"></div>
+        <div><ToastContainer
           position="top-right"
           autoClose={2000}
           hideProgressBar={false}
           closeOnClick={true}
           pauseOnHover={true}
           draggable={true}
-          progress={undefined}
-        />
+          progress={undefined} />
+        </div>
       </div>
     );
   } else {
