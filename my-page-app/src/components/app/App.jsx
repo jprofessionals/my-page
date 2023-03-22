91762a1747cd4c862e803aa7a9384df23625b583
Fredrik Haugseth <fredrik@jpro.no>
2023-03-22 12:35:12 +0100

    Move App.jsx into the components folder

---
+++ my-page-app/src/components/app/App.jsx
@@ -0,0 +1,21 @@
+import React, {useState} from "react";
+import NavBar from "../navbar/NavBar";
+import {User} from "@/User";
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState(new User());
+      return (
+        <>
+          <NavBar logout={logout} user={user}/>
+        </>
+      );
+
+  function logout () {
+    setUser(new User())
+    setIsAuthenticated(false);
+    localStorage.removeItem("user_token");
+  }
+}
+export default App;
