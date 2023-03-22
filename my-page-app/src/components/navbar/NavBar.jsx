cd48e43e437d1cde6aefa17d86514b929cdc832f
Fredrik Haugseth <fredrik@jpro.no>
2023-03-22 12:38:50 +0100

    Update NavBar to use CSS module, and use NextJS Link

--- my-page-app/src/components/navbar/NavBar.jsx
+++ my-page-app/src/components/navbar/NavBar.jsx
@@ -1,26 +1,26 @@
 import { Nav, Navbar } from "react-bootstrap";
-import "./NavBar.scss";
+import styles from "./NavBar.module.scss";
 import jPro_logo_transparent from "../images/jPro_logo_transparent.svg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSignOut } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
 import RequireAdmin from "../../utils/RequireAdmin";
+import Link from "next/link";
 
 const NavBar = ({ logout, user }) => {
 
   return (
-    <Navbar className="navbar" collapseOnSelect expand="sm" variant="dark">
+    <Navbar className={styles.navbar} collapseOnSelect expand="sm" variant="dark">
       <Navbar.Brand href="/" title="min side">
-        <img className="logo" src={jPro_logo_transparent} alt="jPro" />
+        <img className={styles.logo} src={jPro_logo_transparent} alt="jPro" />
       </Navbar.Brand>
       <Navbar.Toggle className="button" aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse className="navtext" id="responsive-navbar-nav">
-        <Nav className="container-fluid">
-          <Link className="nav-link" to="/budsjett">Budsjett</Link>
-          <Link className="nav-link" to="/kalkulator">Lønnskalkulator</Link>
-          <Link className="nav-link" to="/bidra">Bidra til Min side</Link>
+      <Navbar.Collapse className={styles.navtext} id="responsive-navbar-nav">
+        <Nav className={styles.containerFluid}>
+          <Link className={styles.navLink} href="/budsjett">Budsjett</Link>
+          <Link className={styles.navLink} href="/kalkulator">Lønnskalkulator</Link>
+          <Link className={styles.navLink} href="/bidra">Bidra til Min side</Link>
           <RequireAdmin user={user}>
-            <Link className="nav-link" to="/admin">Admin</Link>
+            <Link className={styles.navLink} href="/admin">Admin</Link>
           </RequireAdmin>
           <Nav.Link as="a" href="https://intranet.jpro.no">
             Intranett
@@ -40,7 +40,7 @@ const NavBar = ({ logout, user }) => {
             title="Logg ut" />
           </Nav.Link>
           <Nav.Item style={!user.loaded ? {display: "none"} : {}}>
-            <img className="icon" src={user.icon} alt="Icon"></img>
+            <img className={styles.icon} src={user.icon} alt="Icon"></img>
           </Nav.Item>
         </Nav>
       </Navbar.Collapse>
