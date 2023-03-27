31ba78b0e6fb9606071129e0636a1d8f86afd3d3
Fredrik Haugseth <fredrik@jpro.no>
2023-03-27 15:38:31 +0200

    Add new user type

---
+++ my-page-app/src/types/index.ts
@@ -0,0 +1,11 @@
+export type User = {
+  name: string
+  email: string
+  givenName: string
+  familyName: string
+  icon: string
+  startDate: string
+  admin: string
+  employeeNumber: string
+  loaded: boolean
+}
