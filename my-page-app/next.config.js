a45cac7ef33d1818c925f1793d78d6284028e78c
Fredrik Haugseth <fredrik@jpro.no>
2023-03-27 09:22:50 +0200

    Redirect API calls to backend with Next rewrites

--- my-page-app/next.config.js
+++ my-page-app/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  async rewrites() {
+    return [
+        {
+          source: '/api/:slug*',
+          destination: `http://localhost:8080/api/:slug*`,
+        }
+      ];
+  }
 }
 
 module.exports = nextConfig
