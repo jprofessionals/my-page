8c71fb8bb6ea28f1e7a867478d82fb021b66af7d
Fredrik Haugseth <fredrik@jpro.no>
2023-03-27 15:35:58 +0200

    Use Link tag instead of a tag

--- my-page-app/src/LoggedOut.jsx
+++ my-page-app/src/LoggedOut.jsx
@@ -1,12 +1,13 @@
 import './App.scss'
 import jPro_logo_transparent from './components/images/jPro_logo_transparent.svg'
+import Link from 'next/link'
 
 function LoggedOut() {
   return (
     <div className="vertical-center">
-      <a href="/">
+      <Link href="/">
         <img className="logo" src={jPro_logo_transparent} alt="jPro" />
-      </a>
+      </Link>
       <h2 className="headerText">Du er nå logget ut</h2>
     </div>
   )
