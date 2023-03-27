d67d9648f0898b34f0259967e2d35e5526f4d840
Fredrik Haugseth <fredrik@jpro.no>
2023-03-27 15:44:00 +0200

    Temporarily remove logout, use new auth context, remove title tag

--- my-page-app/src/pages/_app.tsx
+++ my-page-app/src/pages/_app.tsx
@@ -1,10 +1,8 @@
 import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
-import Head from 'next/head'
-import { User } from '@/User'
 import React from 'react'
 import NavBar from '@/components/navbar/NavBar'
-import { AuthProvider } from '@/context/auth'
+import { AuthProvider } from '@/providers/AuthProvider'
 
 export default function App({
   Component,
@@ -12,19 +10,10 @@ export default function App({
 }: AppProps) {
   return (
     <>
-      <Head>
-        <title>Min side</title>
-      </Head>
       <AuthProvider>
-        <NavBar logout={''} />
+        <NavBar />
         <Component {...pageProps} />
       </AuthProvider>
     </>
   )
-
-  function logout() {
-    setUser(new User())
-    setIsAuthenticated(false)
-    localStorage.removeItem('user_token')
-  }
 }
