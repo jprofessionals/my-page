27930a59575602477935a5c0d8d558d172005b5a
Fredrik Haugseth <fredrik@jpro.no>
2023-03-28 12:15:26 +0200

    Add remote image configuration for Google profile icon

--- my-page-app/next.config.js
+++ my-page-app/next.config.js
@@ -10,6 +10,15 @@ const nextConfig = {
       },
     ]
   },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
