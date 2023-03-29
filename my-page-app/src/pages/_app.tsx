84b6f4e4ec80432f778854192996523b4288e264
Fredrik Haugseth <fredrik@jpro.no>
2023-03-29 08:57:04 +0200

    In order to fix hydration error after login, set ssr to false

--- my-page-app/src/pages/_app.tsx
+++ my-page-app/src/pages/_app.tsx
@@ -1,13 +1,16 @@
 import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
 import React from 'react'
-import NavBar from '@/components/navbar/NavBar'
 import { AuthProvider } from '@/providers/AuthProvider'
+import dynamic from 'next/dynamic'
 
 export default function App({
   Component,
   pageProps: { ...pageProps },
 }: AppProps) {
+  const NavBar = dynamic(() => import('@/components/navbar/NavBar'), {
+    ssr: false,
+  })
   return (
     <>
       <AuthProvider>
