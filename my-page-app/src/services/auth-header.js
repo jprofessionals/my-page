2739686dc6e3c329986c509c2d4a02fc20229e60
Fredrik Haugseth <fredrik@jpro.no>
2023-03-29 09:01:20 +0200

    Simplify if check

--- my-page-app/src/services/auth-header.js
+++ my-page-app/src/services/auth-header.js
@@ -1,6 +1,6 @@
 export default function authHeader() {
   const user_token = JSON.parse(localStorage.getItem('user_token'))
-  if (user_token && user_token.id_token) {
+  if (user_token?.id_token) {
     return { Authorization: 'Bearer ' + user_token.id_token }
   } else {
     return {}
