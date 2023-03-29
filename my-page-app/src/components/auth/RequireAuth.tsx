ce2c5152362de4342556a4ddc8a914db2f24063f
Fredrik Haugseth <fredrik@jpro.no>
2023-03-29 08:47:06 +0200

    Move session storage state and related functions into AuthProvider

--- my-page-app/src/components/auth/RequireAuth.tsx
+++ my-page-app/src/components/auth/RequireAuth.tsx
@@ -1,57 +1,9 @@
 import React, { PropsWithChildren } from 'react'
-import ApiService from '../../services/api.service'
-import { toast } from 'react-toastify'
 import Script from 'next/script'
 import { useAuthContext } from '@/providers/AuthProvider'
-import { CredentialResponse } from 'google-one-tap'
 
 function RequireAuth({ children }: PropsWithChildren) {
-  const { isAuthenticated, setIsAuthenticated, setUser } = useAuthContext()
-
-  const authenticate = () => {
-    window?.google?.accounts.id.initialize({
-      client_id: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || '',
-      auto_select: true,
-      prompt_parent_id: 'signInDiv',
-      callback: authHandler,
-    })
-
-    window.google.accounts.id.prompt((notification) => {
-      if (notification.isNotDisplayed() || notification.isSkippedMoment()) {
-        const signInBtn = document.getElementById('signInDiv')
-        if (signInBtn)
-          window.google.accounts.id.renderButton(signInBtn, {
-            type: 'standard',
-            theme: 'outline',
-            size: 'medium',
-            text: 'continue_with',
-            shape: 'rectangular',
-          })
-      }
-    })
-  }
-
-  function authHandler(response: CredentialResponse) {
-    if (response.credential) {
-      setIsAuthenticated(true)
-
-      localStorage.setItem(
-        'user_token',
-        JSON.stringify({ id_token: response.credential }),
-      )
-      ApiService.getUser().then(
-        (response) => {
-          setUser({
-            ...response.data,
-            loaded: true,
-          })
-        },
-        () => {
-          toast.error('Får ikke lastet inn bruker, prøv igjen senere')
-        },
-      )
-    }
-  }
+  const { isAuthenticated, authenticate } = useAuthContext()
 
   if (!isAuthenticated) {
     return (
--- my-page-app/src/providers/AuthProvider.tsx
+++ my-page-app/src/providers/AuthProvider.tsx
@@ -1,9 +1,20 @@
-import { createContext, PropsWithChildren, useContext, useState } from 'react'
+import {
+  createContext,
+  PropsWithChildren,
+  useCallback,
+  useContext,
+  useState,
+} from 'react'
 import { User } from '@/types'
+import { useLocalStorage, useSessionStorage } from 'usehooks-ts'
+import { toast } from 'react-toastify'
+import { CredentialResponse } from 'google-one-tap'
+import ApiService from '@/services/api.service'
 
 type AuthContext = {
   isAuthenticated: boolean
   setIsAuthenticated: (isAuth: boolean) => void
+  authenticate: () => void
   user: User | null
   setUser: (user: User) => void
 }
@@ -11,12 +22,62 @@ type AuthContext = {
 const Context = createContext<AuthContext | null>(null)
 
 export function AuthProvider({ children }: PropsWithChildren) {
-  const [isAuthenticated, setIsAuthenticated] = useState(false)
-  const [user, setUser] = useState<User | null>(null)
+  const [user, setUser] = useSessionStorage<User | null>('user', null)
+  const [isAuthenticated, setIsAuthenticated] = useState(!!user)
 
+  const authenticate = useCallback(() => {
+    window.google.accounts.id.initialize({
+      client_id: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || '',
+      auto_select: true,
+      prompt_parent_id: 'signInDiv',
+      callback: authHandler,
+    })
+
+    window.google.accounts.id.prompt((notification) => {
+      if (notification.isNotDisplayed() || notification.isSkippedMoment()) {
+        const signInDiv = document.getElementById('signInDiv')
+        if (signInDiv)
+          window.google.accounts.id.renderButton(signInDiv, {
+            type: 'standard',
+            theme: 'outline',
+            size: 'medium',
+            text: 'continue_with',
+            shape: 'rectangular',
+          })
+      }
+    })
+  }, [])
+
+  function authHandler(response: CredentialResponse) {
+    if (response.credential) {
+      setIsAuthenticated(true)
+
+      localStorage.setItem(
+        'user_token',
+        JSON.stringify({ id_token: response.credential }),
+      )
+      ApiService.getUser().then(
+        (response) => {
+          setUser({
+            ...response.data,
+            loaded: true,
+          })
+        },
+        () => {
+          toast.error('Får ikke lastet inn bruker, prøv igjen senere')
+        },
+      )
+    }
+  }
   return (
     <Context.Provider
-      value={{ isAuthenticated, setIsAuthenticated, user, setUser }}
+      value={{
+        isAuthenticated,
+        setIsAuthenticated,
+        user,
+        setUser,
+        authenticate,
+      }}
     >
       {children}
     </Context.Provider>
