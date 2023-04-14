075a81a16271757c878e02a2b98e6ef405bb7ef9
Fredrik Haugseth <fredrik@jpro.no>
2023-04-14 12:46:38 +0200

    Implement new methods for creating default budgets for users

--- my-page-api/src/main/kotlin/no/jpro/mypageapi/service/BudgetService.kt
+++ my-page-api/src/main/kotlin/no/jpro/mypageapi/service/BudgetService.kt
@@ -5,6 +5,7 @@ import no.jpro.mypageapi.dto.CreatePostDTO
 import no.jpro.mypageapi.dto.PostDTO
 import no.jpro.mypageapi.dto.UpdatePostDTO
 import no.jpro.mypageapi.entity.Budget
+import no.jpro.mypageapi.entity.BudgetType
 import no.jpro.mypageapi.entity.Post
 import no.jpro.mypageapi.entity.User
 import no.jpro.mypageapi.repository.BudgetRepository
@@ -18,6 +19,8 @@ class BudgetService(
     private val budgetRepository: BudgetRepository,
     private val budgetPostMapper: BudgetPostMapper,
     private val postRepository: PostRepository
+    private val userService: UserService,
+    private val budgetTypeService: BudgetTypeService
 ) {
 
     fun getBudgets(userSub: String): List<BudgetDTO> {
@@ -61,4 +64,66 @@ class BudgetService(
     fun getBudget(budgetId: Long): Budget? {
         return budgetRepository.findBudgetById(budgetId)
     }
+
+    fun createDefaultSetOfBudgets(email: String, startDate: LocalDate) {
+        val user = userService.getUserByEmail(email)
+        if (user != null) {
+            createDefaultSetOfBudgets(user, startDate)
+        }
+    }
+
+    private fun createDefaultSetOfBudgets(user: User, startDate: LocalDate) {
+        val defaultBudgetTypes = getDefaultBudgetTypes()
+        val userHasBudgets = checkIfUserHasAnyBudgetsForGivenBudgetTypes(user, defaultBudgetTypes)
+
+        if (userHasBudgets) {
+            throw RuntimeException("User already has budgets for default budget types")
+        }
+
+        val budgets = createBudgetsFromBudgetTypes(defaultBudgetTypes, user, startDate)
+
+        budgetRepository.saveAll(budgets)
+    }
+
+    private fun createBudgetsFromBudgetTypes(budgetTypes: List<BudgetType>, user: User, startDate: LocalDate): List<Budget> {
+        return budgetTypes.map { budgetType ->
+            Budget(
+                startAmount = budgetType.startAmount,
+                budgetType = budgetType,
+                hours = emptyList(),
+                posts = emptyList(),
+                startDate = startDate,
+                user = user
+            )
+        }
+    }
+
+    private fun getDefaultBudgetTypes(): List<BudgetType> {
+        val budgetTypes = budgetTypeService.getBudgetTypes()
+
+        val defaultBudgetTypes = budgetTypes.filter { budgetType ->
+            budgetType.name == "Kompetanse" ||
+                    budgetType.name == "Hjemmekontor" ||
+                    budgetType.name == "Laptop" ||
+                    budgetType.name == "Mobil"
+        }
+
+        return defaultBudgetTypes
+    }
+
+    private fun checkIfUserHasAnyBudgetsForGivenBudgetTypes(user: User, budgetTypes: List<BudgetType>): Boolean {
+        val budgetsForUser = getBudgetsForUser(user)
+        val budgetTypesForUser = budgetsForUser.map { budget -> budget.budgetType }
+
+        return budgetTypesForUser.any { budgetType -> budgetTypes.contains(budgetType) }
+    }
+
+    private fun getBudgetsForUser(user: User): List<Budget> {
+        if (user.email == null) {
+            throw RuntimeException("Could not get budgets for user with no email")
+        }
+
+        return budgetRepository.findBudgetsByUserEmail(user.email)
+    }
+
 }
