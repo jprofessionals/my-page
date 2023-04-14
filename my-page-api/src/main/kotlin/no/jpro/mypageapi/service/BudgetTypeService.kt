db9905794163cdfc8ab12c56c726a4d89bc6cdac
Fredrik Haugseth <fredrik@jpro.no>
2023-04-14 14:24:10 +0200

    Add Service annotation to BudgetTypeService

--- my-page-api/src/main/kotlin/no/jpro/mypageapi/service/BudgetTypeService.kt
+++ my-page-api/src/main/kotlin/no/jpro/mypageapi/service/BudgetTypeService.kt
@@ -2,7 +2,9 @@ package no.jpro.mypageapi.service
 
 import no.jpro.mypageapi.entity.BudgetType
 import no.jpro.mypageapi.repository.BudgetTypeRepository
+import org.springframework.stereotype.Service
 
+@Service
 class BudgetTypeService(
     private val budgetTypeRepository: BudgetTypeRepository
 ) {
