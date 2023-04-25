381e47d39d551006217cb31c601af62c93840bf9
Fredrik Haugseth <fredrik@jpro.no>
2023-04-25 09:55:36 +0200

    Remove unused repository method

--- my-page-api/src/main/kotlin/no/jpro/mypageapi/repository/Repository.kt
+++ my-page-api/src/main/kotlin/no/jpro/mypageapi/repository/Repository.kt
@@ -1,6 +1,9 @@
 package no.jpro.mypageapi.repository
 
-import no.jpro.mypageapi.entity.*
+import no.jpro.mypageapi.entity.Budget
+import no.jpro.mypageapi.entity.BudgetType
+import no.jpro.mypageapi.entity.Post
+import no.jpro.mypageapi.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
@@ -21,7 +24,6 @@ interface BudgetTypeRepository : JpaRepository<BudgetType, Long> {
 interface BudgetRepository : JpaRepository<Budget, Long> {
     fun findBudgetsByUserSub(userSub: String): List<Budget>
     fun findBudgetsByUserEmployeeNumber(userEmployeeNumber: Int): List<Budget>
-    fun findBudgetsByUserEmail(userEmail: String): List<Budget>
     fun findBudgetById(budgetId: Long): Budget
     fun findBudgetsByUserEmailAndBudgetTypeIn(userEmail: String, budgetTypes: List<BudgetType>): List<Budget>
 }
