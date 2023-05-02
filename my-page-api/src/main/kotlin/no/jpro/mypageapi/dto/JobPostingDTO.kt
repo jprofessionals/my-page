5a8e46b9d6584bcc847503b1bdb5ad41bb7a9462
Fredrik Haugseth <fredrik@jpro.no>
2023-05-02 17:18:12 +0200

    Rename technologies field to tags

--- my-page-api/src/main/kotlin/no/jpro/mypageapi/dto/JobPostingDTO.kt
+++ my-page-api/src/main/kotlin/no/jpro/mypageapi/dto/JobPostingDTO.kt
@@ -7,7 +7,7 @@ data class JobPostingDTO(
     val title: String,
     val description: String?,
     val customer: String,
-    val technologies: List<String>?,
+    val tags: List<String>?,
     val location: String?,
     val dueDateForApplication: LocalDate?,
     val requiredYearsOfExperience: Int?,
