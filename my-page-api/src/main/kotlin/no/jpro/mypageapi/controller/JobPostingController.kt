d8353699834fedc96c351ed988279aaa48f9b811
Fredrik Haugseth <fredrik@jpro.no>
2023-05-03 10:55:29 +0200

    Update controller with logging

--- my-page-api/src/main/kotlin/no/jpro/mypageapi/controller/JobPostingController.kt
+++ my-page-api/src/main/kotlin/no/jpro/mypageapi/controller/JobPostingController.kt
@@ -7,6 +7,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import no.jpro.mypageapi.dto.JobPostingDTO
 import no.jpro.mypageapi.service.JobPostingService
+import no.jpro.mypageapi.utils.mapper.JobPostingMapper
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -15,8 +17,11 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("jobposting")
 @SecurityRequirement(name = "Bearer Authentication")
 class JobPostingController(
-    private val jobPostingService: JobPostingService
+    private val jobPostingService: JobPostingService,
+    private val jobPostingMapper: JobPostingMapper
 ) {
+
+    private val logger = LoggerFactory.getLogger(JobPostingController::class.java)
 
     @GetMapping
     @Operation(summary = "Get all job postings")
@@ -26,7 +31,12 @@ class JobPostingController(
         content = [Content(schema = Schema(implementation = JobPostingDTO::class))]
     )
     fun getAllJobPostings(): List<JobPostingDTO> {
-        return jobPostingService.getAllJobPostings()
+        logger.info("Getting all job postings")
+
+        val jobPostingDTOList = jobPostingService.getAllJobPostings().map { jobPostingMapper.toJobPostingDTO(it) }
+        logger.info("Got the following job postings: $jobPostingDTOList")
+
+        return jobPostingDTOList
     }
 
 }
