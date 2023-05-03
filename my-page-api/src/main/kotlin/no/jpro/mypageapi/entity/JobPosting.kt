5eaf6cfe738e2194a4072195bfc8557de806db5e
Fredrik Haugseth <fredrik@jpro.no>
2023-05-03 10:48:56 +0200

    Add new entities for JobPosting and JobPostingTag

---
+++ my-page-api/src/main/kotlin/no/jpro/mypageapi/entity/JobPosting.kt
@@ -0,0 +1,27 @@
+package no.jpro.mypageapi.entity
+
+import jakarta.persistence.*
+import java.time.LocalDate
+
+@Entity
+class JobPosting(
+    val title: String,
+    val description: String?,
+    val customer: String,
+    val location: String?,
+    val dueDateForApplication: LocalDate?,
+    val requiredYearsOfExperience: Int?,
+    val resourcesNeeded: Int?,
+    @ManyToMany(
+        cascade = [CascadeType.PERSIST, CascadeType.MERGE],
+    )
+    @JoinTable(
+        name = "job_posting_tags",
+        joinColumns = [JoinColumn(name = "job_posting_id", referencedColumnName = "id")],
+        inverseJoinColumns = [JoinColumn(name = "tag_id", referencedColumnName = "id")]
+    )
+    val tags: Set<JobPostingTag> = HashSet(),
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null
+)
