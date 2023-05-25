c9eae39050597bc03e729cdb0c82d8273efa7e3e
Marius Kluften <mkluften93@gmail.com>
2023-05-25 21:34:47 +0200

    Restructure post component

--- my-page-app/src/components/budget/Post.tsx
+++ my-page-app/src/components/budget/Post.tsx
@@ -7,6 +7,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrash, faEdit } from '@fortawesome/free-solid-svg-icons'
 import { Budget } from '@/types'
 import styles from './Post.module.scss'
+import getInNok from '@/utils/getInNok'
+import clsx from 'clsx'
 
 type Props = {
   budget: Budget
@@ -38,95 +40,66 @@ const Post = ({ refreshBudgets, post, budget }: Props) => {
       setIsEditPostOpen(true)
     }
   }
+  if (isEditPostOpen)
+    return (
+      <EditBudgetPost
+        toggle={togglerEdit}
+        refreshBudgets={refreshBudgets}
+        budget={budget}
+        post={post}
+        setIsLoadingEditPost={setIsLoadingEditPost}
+        isLoadingEditPost={isLoadingEditPost}
+      />
+    )
   return (
-    <div className="post">
-      <Card
-        border={postInFuture() ? 'grey' : 'dark'}
-        style={isEditPostOpen ? { display: 'none' } : {}}
-      >
-        <div>
-          <Card.Header>
-            <div className={styles.headerPost}>
-              <b>{post.description}</b>
-              <div className={styles.rightBtnsDiv}>
-                <div
-                  style={
-                    isLoadingDeletePost || post.locked
-                      ? { display: 'none' }
-                      : {}
-                  }
-                >
-                  <Button
-                    className="leftBtn"
-                    type="button"
-                    title="Rediger post"
-                    onClick={togglerEdit}
-                  >
-                    <div title="Rediger Post">
-                      <div style={isLoadingEditPost ? { display: 'none' } : {}}>
-                        <FontAwesomeIcon icon={faEdit} />
-                      </div>
-                      <div style={isLoadingEditPost ? {} : { display: 'none' }}>
-                        <Spinner animation="border" size="sm" />
-                      </div>
-                    </div>
-                  </Button>
+    <div className="overflow-hidden w-full rounded-xl border-2 border-gray-500 border-solid shadow-sm">
+      <div className="flex justify-between items-center p-3 pb-2 w-full text-sm bg-gray-200">
+        <span className="p2">{post.description}</span>
+
+        <div className="btn-group">
+          {isLoadingDeletePost || true ? (
+            <button
+              className="btn btn-primary btn-sm"
+              type="button"
+              title="Rediger post"
+              onClick={togglerEdit}
+            >
+              <div title="Rediger Post">
+                <div style={isLoadingEditPost ? { display: 'none' } : {}}>
+                  Endre
                 </div>
-                <div
-                  style={
-                    isLoadingEditPost || post.locked ? { display: 'none' } : {}
-                  }
-                >
-                  <Button
-                    className="removePostBtn"
-                    type="button"
-                    title="Slett post"
-                    onClick={toggler}
-                  >
-                    <DeleteBudgetPostModal
-                      isDeleteModalOpen={isDeleteModalOpen}
-                      toggler={toggler}
-                      refreshBudgets={refreshBudgets}
-                      post={post}
-                      setIsLoadingDeletePost={setIsLoadingDeletePost}
-                    />
-                    <div style={isLoadingDeletePost ? { display: 'none' } : {}}>
-                      <FontAwesomeIcon icon={faTrash} />
-                    </div>
-                    <div style={isLoadingDeletePost ? {} : { display: 'none' }}>
-                      <Spinner animation="border" size="sm" />
-                    </div>
-                  </Button>
+                <div style={isLoadingEditPost ? {} : { display: 'none' }}>
+                  <Spinner animation="border" size="sm" />
                 </div>
               </div>
-            </div>
-          </Card.Header>
+            </button>
+          ) : null}
+
+          {isLoadingEditPost || true ? (
+            <button
+              className="text-white btn btn-error btn-sm"
+              type="button"
+              title="Slett post"
+              onClick={toggler}
+            >
+              <DeleteBudgetPostModal
+                isDeleteModalOpen={isDeleteModalOpen}
+                toggler={toggler}
+                refreshBudgets={refreshBudgets}
+                post={post}
+                setIsLoadingDeletePost={setIsLoadingDeletePost}
+              />
+              <div className={clsx(!isLoadingEditPost || 'hidden')}>Slett</div>
+              <div className={clsx(isLoadingDeletePost || 'hidden')}>
+                <Spinner animation="border" size="sm" />
+              </div>
+            </button>
+          ) : null}
         </div>
-        <Card.Body>
-          <ul className={styles.postList}>
-            <li>
-              <b>Pris:</b>{' '}
-              {post.amountExMva.toLocaleString('no-NO', {
-                maximumFractionDigits: 2,
-                style: 'currency',
-                currency: 'NOK',
-              })}
-            </li>
-            <li>
-              <b>Dato:</b> {Moment(post.date).format('DD.MM.YYYY')}
-            </li>
-          </ul>
-        </Card.Body>
-      </Card>
-      <div style={isEditPostOpen ? {} : { display: 'none' }}>
-        <EditBudgetPost
-          toggle={togglerEdit}
-          refreshBudgets={refreshBudgets}
-          budget={budget}
-          post={post}
-          setIsLoadingEditPost={setIsLoadingEditPost}
-          isLoadingEditPost={isLoadingEditPost}
-        />
+      </div>
+      <div className="flex flex-col p-3">
+        <b>Pris:</b> {getInNok(post.amountExMva)}
+        <b>Dato:</b> {Moment(post.date).format('DD.MM.YYYY')}
       </div>
     </div>
   )
