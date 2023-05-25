cf40f0d337739d590a417df2975cf55e73e1564f
Marius Kluften <mkluften93@gmail.com>
2023-05-25 14:59:35 +0200

    replace styling with tw on items and sync design with proposed design

--- my-page-app/src/components/budget/BudgetItem.tsx
+++ my-page-app/src/components/budget/BudgetItem.tsx
@@ -1,24 +1,74 @@
 import Post from './Post'
-import styles from './BudgetItem.module.scss'
 import CreateBudgetPost from './CreateBudgetPost'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlus, faRemove } from '@fortawesome/free-solid-svg-icons'
-import { useContext, useEffect, useState } from 'react'
+import {
+  IconDefinition,
+  faChevronDown,
+  faGraduationCap,
+  faHome,
+  faLaptop,
+  faMoneyBill,
+  faPhone,
+  faPlus,
+} from '@fortawesome/free-solid-svg-icons'
+import { useState } from 'react'
 import RequireAdmin from '../../utils/RequireAdmin'
-import { Accordion, AccordionContext, Button } from 'react-bootstrap'
 import BudgetInformation from './BudgetInformation'
 import { Budget } from '@/types'
+import getInNok from '@/utils/getInNok'
+import clsx from 'clsx'
 
 type Props = {
   budget: Budget
+  isActive: boolean
   refreshBudgets: any
+  type: 'list' | 'tiles'
 }
 
-const BudgetItem = ({ budget, refreshBudgets }: Props) => {
-  const postList = budget.posts
-  const [posts, setPosts] = useState<any[]>([])
+const budgetConfigs: Record<
+  string,
+  {
+    bgColor: string
+    textColor: string
+    icon: IconDefinition | null
+  }
+> = {
+  Kompetanse: {
+    bgColor: 'bg-green-600',
+    textColor: 'text-white',
+    icon: faGraduationCap,
+  },
+  Mobil: {
+    bgColor: 'bg-slate-600',
+    textColor: 'text-white',
+    icon: faPhone,
+  },
+  Laptop: {
+    bgColor: 'bg-orange-600',
+    textColor: 'text-white',
+    icon: faLaptop,
+  },
+  Hjemmekontor: {
+    bgColor: 'bg-slate-950',
+    textColor: 'text-white',
+    icon: faHome,
+  },
+  Bruttotrekk: {
+    bgColor: 'bg-stone-200',
+    textColor: 'text-black',
+    icon: faMoneyBill,
+  },
+  default: {
+    bgColor: 'bg-slate-100',
+    textColor: 'text-black',
+    icon: null,
+  },
+}
+
+const BudgetItem = ({ budget, refreshBudgets, type, isActive }: Props) => {
+  const { posts } = budget
   const [cardItem, setCardItem] = useState<any>()
-  const { activeEventKey } = useContext(AccordionContext)
+  const [isOpen, setIsOpen] = useState(isActive)
 
   const toggler = (e: any) => {
     if (!cardItem) {
@@ -36,62 +86,94 @@ const BudgetItem = ({ budget, refreshBudgets }: Props) => {
     }
   }
 
-  useEffect(() => {
-    setPosts(postList)
-  }, [postList])
+  const { bgColor, textColor, icon } =
+    type === 'tiles'
+      ? budgetConfigs[budget.budgetType.name]
+      : budgetConfigs.default
 
   return (
-    <Accordion.Item key={budget.id} eventKey={budget.id}>
-      <Accordion.Header title={activeEventKey === budget.id ? 'Lukk' : 'Åpne'}>
-        <ul className={styles.initialBudgetInformation}>
-          <li>
-            <span title="Type budsjett">{budget.budgetType.name}</span>
-          </li>
-          <li>
-            <span title="Saldo">
-              Saldo:{' '}
-              {budget.balance.toLocaleString('no-NO', {
-                maximumFractionDigits: 2,
-                style: 'currency',
-                currency: 'NOK',
-              })}
-            </span>
-          </li>
-        </ul>
-      </Accordion.Header>
-      <Accordion.Body>
-        <BudgetInformation budget={budget} />
-        <div className={styles.posts}>
-          <div className={styles.header}>
-            <h3>Historikk</h3>
-            <RequireAdmin>
-              <Button onClick={toggler} className="plus shadow-none">
-                <FontAwesomeIcon
-                  className="toggleButton"
-                  icon={cardItem ? faRemove : faPlus}
-                  title={cardItem ? 'Avbryt' : 'Legg til ny post'}
-                />
-              </Button>
-            </RequireAdmin>
-          </div>
-          {cardItem}
-          <span style={posts.length > 0 ? { display: 'none' } : {}}>
-            Ingen historikk funnet
-          </span>
-          {posts
-            .sort((a, b) => (a.date < b.date ? 1 : -1))
-            .map((post) => (
-              <Post
-                // className="post"
-                key={post.id}
-                post={post}
-                budget={budget}
-                refreshBudgets={refreshBudgets}
-              />
-            ))}
+    <div
+      style={{
+        margin: 0,
+      }}
+      className={clsx(
+        'collapse show card flex card-bordered self-start shadow-lg',
+        {
+          'first:rounded-t-lg last:rounded-b-lg rounded-none': type === 'list',
+        },
+        isOpen ? 'collapse-open' : '',
+      )}
+    >
+      <button
+        className={clsx(
+          'grid grid-cols-2 items-center pr-6 collapse-title self-start hover:brightness-90 focus:brightness-90',
+          textColor,
+          bgColor,
+        )}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <span title="Type budsjett" className="flex gap-4 uppercase">
+          {icon ? (
+            <FontAwesomeIcon icon={icon} size="xl" className="w-8" />
+          ) : null}
+          {budget.budgetType.name}{' '}
+        </span>
+        <div className="flex justify-between">
+          <span title="Saldo">Saldo: {getInNok(budget.balance)}</span>
+          <FontAwesomeIcon
+            icon={faChevronDown}
+            className={clsx(
+              isOpen ? 'rotate-180' : 'rotate-0',
+              'place-self-end self-center',
+            )}
+          />
         </div>
-      </Accordion.Body>
-    </Accordion.Item>
+      </button>
+      <div className="collapse-content">
+        {isOpen ? (
+          <>
+            <BudgetInformation budget={budget} />
+            <div className="flex flex-col gap-3">
+              <div className="flex justify-between items-center py-3 pl-3">
+                <h3 className="text-xl font-bold">Historikk</h3>
+                <RequireAdmin>
+                  <button
+                    onClick={toggler}
+                    className="text-xl rounded-full btn btn-info btn-sm"
+                  >
+                    <FontAwesomeIcon
+                      className={clsx(
+                        cardItem ? 'rotate-45' : '',
+                        'transition-all',
+                      )}
+                      icon={faPlus}
+                      title={cardItem ? 'Avbryt' : 'Legg til ny post'}
+                    />
+                  </button>
+                </RequireAdmin>
+              </div>
+              {cardItem}
+              {posts.length > 0 ? (
+                posts
+                  .sort((a, b) => (a.date < b.date ? 1 : -1))
+                  .map((post) => (
+                    <Post
+                      key={post.id}
+                      post={post}
+                      budget={budget}
+                      refreshBudgets={refreshBudgets}
+                    />
+                  ))
+              ) : (
+                <span style={posts.length > 0 ? { display: 'none' } : {}}>
+                  Ingen historikk funnet
+                </span>
+              )}
+            </div>
+          </>
+        ) : null}
+      </div>
+    </div>
   )
 }
 
