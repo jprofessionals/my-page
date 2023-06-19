1f1fb98ec055b4e50835a5b0f3968aec7a700dd2
ulrikror99 <ulrik@jpro.no>
2023-06-19 08:35:08 +0200

    Implementerte deductiondate (trekkdato)

--- my-page-app/src/components/budget/Post.tsx
+++ my-page-app/src/components/budget/Post.tsx
@@ -30,58 +30,74 @@ const Post = ({ refreshBudgets, post, budget, showActions }: Props) => {
   }
   if (isEditPostOpen)
     return (
-      <EditBudgetPost
-        toggle={togglerEdit}
-        refreshBudgets={refreshBudgets}
-        budget={budget}
-        post={post}
-        setIsLoadingEditPost={setIsLoadingEditPost}
-        isLoadingEditPost={isLoadingEditPost}
-      />
+        <EditBudgetPost
+            toggle={togglerEdit}
+            refreshBudgets={refreshBudgets}
+            budget={budget}
+            post={post}
+            setIsLoadingEditPost={setIsLoadingEditPost}
+            isLoadingEditPost={isLoadingEditPost}
+        />
     )
+
+  const checkDateOfDeduction = () => {
+    if (post.dateOfDeduction) {
+      // Check if dateOfDeduction exists and is not null
+      return (
+          <div className="flex flex-col">
+            <b>Trekkdato:</b> {moment(post.dateOfDeduction).format('DD.MM.YYYY')}
+          </div>
+      );
+    } else {
+      // Handle the case when dateOfDeduction is null or undefined
+      return
+    }
+  };
+
   return (
-    <div className="overflow-hidden w-full rounded-xl border-2 border-gray-500 border-solid shadow-sm">
-      <div className="flex justify-between items-center p-3 pb-2 w-full text-sm bg-gray-200">
+      <div className="overflow-hidden w-full rounded-xl border-2 border-gray-500 border-solid shadow-sm">
+        <div className="flex justify-between items-center p-3 pb-2 w-full text-sm bg-gray-200">
         <span className="flex gap-2 items-center p-1">
           {showActions ? (
-            <FontAwesomeIcon icon={post.locked ? faLock : faLockOpen} />
+              <FontAwesomeIcon icon={post.locked ? faLock : faLockOpen} />
           ) : null}
           {post.description}
         </span>
 
-        {showActions ? (
-          <div className="btn-group">
-            <button
-              className="btn btn-primary btn-sm"
-              type="button"
-              title="Rediger post"
-              onClick={togglerEdit}
-              disabled={post.locked}
-            >
-              <div title="Rediger Post">
-                {isLoadingEditPost ? (
-                  <FontAwesomeIcon
-                    icon={faRefresh}
-                    className="animate-spin"
-                    size="sm"
-                  />
-                ) : (
-                  'Endre'
-                )}
+          {showActions ? (
+              <div className="btn-group">
+                <button
+                    className="btn btn-primary btn-sm"
+                    type="button"
+                    title="Rediger post"
+                    onClick={togglerEdit}
+                    disabled={post.locked}
+                >
+                  <div title="Rediger Post">
+                    {isLoadingEditPost ? (
+                        <FontAwesomeIcon
+                            icon={faRefresh}
+                            className="animate-spin"
+                            size="sm"
+                        />
+                    ) : (
+                        'Endre'
+                    )}
+                  </div>
+                </button>
+                <DeleteBudgetPostModal
+                    refreshBudgets={refreshBudgets}
+                    post={post}
+                />
               </div>
-            </button>
-            <DeleteBudgetPostModal
-              refreshBudgets={refreshBudgets}
-              post={post}
-            />
-          </div>
-        ) : null}
+          ) : null}
+        </div>
+        <div className="flex flex-col p-3">
+          <b>Pris:</b> {getInNok(post.amountExMva)}
+          <b>Dato:</b> {moment(post.date).format('DD.MM.YYYY')}
+          {checkDateOfDeduction()}
+        </div>
       </div>
-      <div className="flex flex-col p-3">
-        <b>Pris:</b> {getInNok(post.amountExMva)}
-        <b>Dato:</b> {moment(post.date).format('DD.MM.YYYY')}
-      </div>
-    </div>
   )
 }
 
