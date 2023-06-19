2b988b0f2c68f2a2c15998b304d3bf499a332ac9
ulrikror99 <ulrik@jpro.no>
2023-06-19 10:18:49 +0200

    Format fix

--- my-page-app/src/config/config.ts
+++ my-page-app/src/config/config.ts
@@ -1,16 +1,18 @@
 interface Config {
-    googleClientId: string;
+  googleClientId: string
 }
 
 export default function config(): Config {
-    console.log(window.location)
-    if (window.location.hostname === 'minside.jpro.no') {
-        return {
-            googleClientId: '742259097135-amdq1io2p7t7e4jud14o4deaokp5acl1.apps.googleusercontent.com'
-        }
-    } else {
-        return {
-            googleClientId: '365277685106-agts9g8tvljvdflgqottt52ujp8p989d.apps.googleusercontent.com'
-        }
+  console.log(window.location)
+  if (window.location.hostname === 'minside.jpro.no') {
+    return {
+      googleClientId:
+        '742259097135-amdq1io2p7t7e4jud14o4deaokp5acl1.apps.googleusercontent.com',
     }
-};
+  } else {
+    return {
+      googleClientId:
+        '365277685106-agts9g8tvljvdflgqottt52ujp8p989d.apps.googleusercontent.com',
+    }
+  }
+}
