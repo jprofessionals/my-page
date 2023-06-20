5d4fe9a730f9bb74c2907c6ea148eab062802278
Iver-Jpro <iver@jpro.no>
2023-06-20 08:32:54 +0200

    Fix for nextjs error

--- my-page-app/src/pages/ai.tsx
+++ my-page-app/src/pages/ai.tsx
@@ -1,21 +1,22 @@
+import Link from "next/link";
+
 function AI() {
     return (
 
         <div className="p-4 prose" data-theme="jpro">
             <h2>AI tjenester</h2>
 
-                Her finner du tjenester og verktøy basert på kunstig intelligens utviklet i JPro.
-                <br/>
+            Her finner du tjenester og verktøy basert på kunstig intelligens utviklet i JPro.
+            <br/>
 
-                <p>
-                <a
-                    href="/explorer"
-                    className="text-warning"
-                >
-                    AI explorer.
-                </a>
+
+            <p>
+                <Link href="/explorer">
+                    <a className="text-warning">
+                        AI explorer.
+                    </a>
+                </Link>
                 &nbsp;Her kan du reise verden rundt med kunstig intelligens.
-
             </p>
         </div>
 
