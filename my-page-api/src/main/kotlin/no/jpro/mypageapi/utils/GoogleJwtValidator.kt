1adb9dcff7afa126f47c32553eea30f9535ce01d
Iver-Jpro <iver@jpro.no>
2023-06-20 14:38:08 +0200

    Tar bort tokenvaliering i websocket på gcp

--- my-page-api/src/main/kotlin/no/jpro/mypageapi/utils/GoogleJwtValidator.kt
+++ my-page-api/src/main/kotlin/no/jpro/mypageapi/utils/GoogleJwtValidator.kt
@@ -7,17 +7,32 @@ import com.google.api.client.http.javanet.NetHttpTransport
 import com.google.api.client.json.JsonFactory
 import com.google.api.client.json.gson.GsonFactory
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
 import java.util.*
 
+interface GoogleJwtValidator {
+    fun isValidToken(token: String): Boolean
+}
+
 @Component
-class GoogleJwtValidator(@Value("\${google.clientid}") clientId: String) {
+@Profile("local", "h2")
+class GoogleJwtValidatorImpl(@Value("\${google.clientid}") clientId: String) : GoogleJwtValidator {
     private val transport: HttpTransport = NetHttpTransport()
     private val jsonFactory: JsonFactory = GsonFactory()
     private val verifier = GoogleIdTokenVerifier.Builder(transport, jsonFactory).setAudience(Collections.singletonList(clientId)).build()
 
-    fun isValidToken(token: String): Boolean {
+    override fun isValidToken(token: String): Boolean {
         val idToken: GoogleIdToken = verifier.verify(token) ?: return false
         return (idToken.payload?.emailVerified == true && idToken.payload?.email?.endsWith("@jpro.no") == true)
+    }
+}
+
+@Component
+@Profile("gcp")
+class GoogleJwtValidatorDummy : GoogleJwtValidator {
+    override fun isValidToken(token: String): Boolean {
+        //TODO: midlertidig validator intill vi får dette til å virke i GCP
+        return true
     }
 }
