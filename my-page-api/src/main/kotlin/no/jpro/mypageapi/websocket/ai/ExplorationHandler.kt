890a87f27d13552e4ac9bbc4c795f6a99dbcc74e
Iver-Jpro <iver@jpro.no>
2023-06-20 10:14:30 +0200

    token feilsøking

--- my-page-api/src/main/kotlin/no/jpro/mypageapi/websocket/ai/ExplorationHandler.kt
+++ my-page-api/src/main/kotlin/no/jpro/mypageapi/websocket/ai/ExplorationHandler.kt
@@ -8,19 +8,23 @@ import org.springframework.web.socket.CloseStatus
 import org.springframework.web.socket.TextMessage
 import org.springframework.web.socket.WebSocketSession
 import org.springframework.web.socket.handler.TextWebSocketHandler
+import java.util.logging.Logger
 
 
 class ExplorationHandler(val explorationService: ExplorationService, val googleJwtValidator: GoogleJwtValidator) : TextWebSocketHandler() {
-
+    private val logger = Logger.getLogger(ExplorationService::class.java.name)
     override fun handleTextMessage(session: WebSocketSession, message: TextMessage) {
         val payload = message.payload
 
         if (session.attributes["token"] == null) {
-
+            session.close(CloseStatus(CloseStatus.PROTOCOL_ERROR.code, "No token received"))
+            return
+//TODO: don't return
             // verify the token and store it in the session or close the connection if the token is not valid
             if (googleJwtValidator.isValidToken(payload)) {
                 session.attributes["token"] = payload
             } else {
+                logger.log(java.util.logging.Level.WARNING, "Invalid token received, closing connection")
                 session.close(CloseStatus(CloseStatus.PROTOCOL_ERROR.code, "Invalid token"))
             }
         } else if(payload == "reset") {
