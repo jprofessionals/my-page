d11ebd727be70aeeac8729cc4aaaf09e4da248a6
ulrikror99 <ulrik@jpro.no>
2023-06-21 11:16:46 +0200

    Added month and year buttons & changed navbar name

---
+++ my-page-app/src/components/hyttebooking/yearOverview.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Calendar } from '@/components/ui/calendar'
+
+
+
+export default function yearOverview(){
+
+    return(
+        <div>
+            Halllllo
+        </div>
+    );
+}
