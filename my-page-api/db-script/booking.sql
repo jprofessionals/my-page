2b5b5878df17fe6abdfe798ad08e31809ae9ff98
Sophie Xu <sophiexu@Sophies-MacBook-Pro.local>
2023-06-26 11:01:13 +0200

    Changed the dummy data value of the employee_id to some number that hasnt been used by real employees.

--- my-page-api/db-script/booking.sql
+++ my-page-api/db-script/booking.sql
@@ -1,3 +1,3 @@
-INSERT INTO mypage.booking (employee_id, apartment_id, start_date, end_date) VALUES (1, 1, current_date, current_date+1);
-INSERT INTO mypage.booking (employee_id, apartment_id, start_date, end_date) VALUES (2, 2, current_date, current_date+1);
-INSERT INTO mypage.booking (employee_id, apartment_id, start_date, end_date) VALUES (1, 3, current_date, current_date+1);
+INSERT INTO mypage.booking (employee_id, apartment_id, start_date, end_date) VALUES (1000000, 1, current_date, current_date+1);
+INSERT INTO mypage.booking (employee_id, apartment_id, start_date, end_date) VALUES (2000000, 2, current_date, current_date+1);
+INSERT INTO mypage.booking (employee_id, apartment_id, start_date, end_date) VALUES (1000000, 3, current_date, current_date+1);
