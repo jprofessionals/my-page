b8f11807ee8dbe6f33e7b37ef010aaf223b04e25
Sophie Xu <sophiexu@Sophies-MacBook-Pro.local>
2023-08-08 15:55:12 +0200

    Removed moment import

--- my-page-app/src/components/hyttebooking/EditBooking.tsx
+++ my-page-app/src/components/hyttebooking/EditBooking.tsx
@@ -1,6 +1,5 @@
 import { ChangeEvent, useState } from 'react'
 import { API_URL } from '../../services/api.service'
-import moment from 'moment'
 import { toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import Loading from '@/components/Loading'
