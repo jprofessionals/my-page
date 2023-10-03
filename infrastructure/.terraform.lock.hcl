dcef55faa0a37f2d06643c3370d75e3dd04851e2
Iver-Jpro <iver@jpro.no>
2023-10-03 10:10:53 +0200

    Restored infrastructure directory and contents

---
+++ infrastructure/.terraform.lock.hcl
@@ -0,0 +1,81 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/google" {
+  version = "4.67.0"
+  hashes = [
+    "h1:FYSnhl0j80IsT2st63BZAu/cP17W1pEsX1OhLQZ1Muc=",
+    "zh:0cdc7b724f115e9456e6c403c88ba68c36509bb92a717d20db3099b2e6c29968",
+    "zh:0feb6f8c5d9c064bf614cbe1bc1cc46aaf992bc1c8a95212a9d1ddad3da03d0a",
+    "zh:626503789c45350f7caed95fc02e67e505a582539c7fc5a1c6a4f3fb662534fc",
+    "zh:63b1dbc6c223edbdac45cd35e7222c8475d60359d2c1bf50d496f65bc003e3cc",
+    "zh:7b0e4386ae0e53e77ffaba18bdc482111a6aacf8b536cdf255e4bdc0056be91b",
+    "zh:7c48f857071c04efa7afb23bd11752d652ded4cbdf747a5f512dec828fcaba4c",
+    "zh:86c62e8332739d453502f268c3b8eac27454436ec56af2e95642ca18d9bf3502",
+    "zh:8748472a4c15dca832757ed5d9b618950bc0f9c36aadc1556fa2b94cb9c77225",
+    "zh:9c4314e89077e1656a3a54b8ef8648fdb0897b6658570d52dce2a53108829b55",
+    "zh:d506533c2961ae74e65e7908efc34148097470009851352bb77d240375fa628e",
+    "zh:e4068daf384b8ce45e79005d5696271c135546a245cf47554370d0b60c9ac93f",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/google-beta" {
+  version     = "4.67.0"
+  constraints = ">= 4.51.0, != 4.65.0, != 4.65.1, < 5.0.0"
+  hashes = [
+    "h1:P2ITUpS8Tmr9pkesfdbwyH7skutoA4zw2AXVhXFC6n4=",
+    "zh:43bfc5e9f2425643688af98d6a8a961d5423e5ab6210516ff1abd745c45f39cc",
+    "zh:6b3a2cde4b030de17f4c74c96b18ca2dee4eac49ca0a8f6f6239204ae6e15e00",
+    "zh:6e036490b437e6b8e075eb008496092c8ac1f33f626154d7e3f3c8ba9566aa54",
+    "zh:726bd9ce22e6ed6efab3d3e77bf97692eafae563d82491a15c7052a8f4feb4d3",
+    "zh:76a0a2b944930d7f476244d6cd79bd6582a80ba79db836a742bf4c09820b0bb2",
+    "zh:986c25a405f678565c1d468729260d1b0094f12a9abefa2ad6ff00b0eb26b8bb",
+    "zh:997982ece9ff0936d79ec70c2d50b301be8c072c873ce33d7ffbc72c70ff1f72",
+    "zh:bd362367c376c25e3b0f530b0c704ecc58b5b776e0a25a8949b28fda45e1f411",
+    "zh:cd44f611399e68c0b0e26830ab80919cb0cdb0198b07b34d48fcf0f30f8e5abb",
+    "zh:ea5b427464a4c1263b19b69a4f6b53aa442851dd9d2d285b7cfd3fb8d0b16dee",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+    "zh:f8486bf41df207a6ce59bb30c6f6e6dfaf87225f11cbb6fd332bfc0254193469",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/kubernetes" {
+  version     = "2.20.0"
+  constraints = "~> 2.10"
+  hashes = [
+    "h1:E7VAZorKe5oXn6h1nxP3ROwWNiQSrZlTawzix1sh8kM=",
+    "zh:30bc224c94d2c90a7d44554f2ad30e3b62c7ffc6ddb7d4fd31b9acafb8b5ad77",
+    "zh:3903cc9f0c3169a24265c4920d925ed7e37cbc4312237b29bd5b4ddcd6bdc535",
+    "zh:512240f6dad36c0116a8717487a4ea12a6b4191028782c5b6749037892e2c6ed",
+    "zh:57d5f77dcde7781803b465205aec3507780bfaa77031f5b893ae7cbebd4789b6",
+    "zh:6274ab8c3b59634c344c337218223640e9d954996b9299587ca924e4dfb77aa4",
+    "zh:6d838a25f3e3c696cf894f0adb44b41b461a2c76f914f1ae2c318ccbb1ec4e36",
+    "zh:92f09e3e03311c4e24601b704d85de57677f49e29f42cc3479fafa68f5de300a",
+    "zh:abb3cd606e485a46c076d6f60d37b5e5ecaa128c0150c8235627b484f2fac902",
+    "zh:afc07f5c0d7ce2cc907600e4f87a1290203a36221951e19e5d3f1409a0502377",
+    "zh:d9c01e4f12fabf5d6d9d11ceb409585b71c2abcad478496446de6ff18bbf2f5f",
+    "zh:f40faba2269184b305f229503945400ed6eeafec7ac395c23f243bccab7b11b2",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/random" {
+  version     = "3.5.1"
+  constraints = ">= 2.1.0"
+  hashes = [
+    "h1:IL9mSatmwov+e0+++YX2V6uel+dV6bn+fC/cnGDK3Ck=",
+    "zh:04e3fbd610cb52c1017d282531364b9c53ef72b6bc533acb2a90671957324a64",
+    "zh:119197103301ebaf7efb91df8f0b6e0dd31e6ff943d231af35ee1831c599188d",
+    "zh:4d2b219d09abf3b1bb4df93d399ed156cadd61f44ad3baf5cf2954df2fba0831",
+    "zh:6130bdde527587bbe2dcaa7150363e96dbc5250ea20154176d82bc69df5d4ce3",
+    "zh:6cc326cd4000f724d3086ee05587e7710f032f94fc9af35e96a386a1c6f2214f",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:b6d88e1d28cf2dfa24e9fdcc3efc77adcdc1c3c3b5c7ce503a423efbdd6de57b",
+    "zh:ba74c592622ecbcef9dc2a4d81ed321c4e44cddf7da799faa324da9bf52a22b2",
+    "zh:c7c5cde98fe4ef1143bd1b3ec5dc04baf0d4cc3ca2c5c7d40d17c0e9b2076865",
+    "zh:dac4bad52c940cd0dfc27893507c1e92393846b024c5a9db159a93c534a3da03",
+    "zh:de8febe2a2acd9ac454b844a4106ed295ae9520ef54dc8ed2faf29f12716b602",
+    "zh:eab0d0495e7e711cca367f7d4df6e322e6c562fc52151ec931176115b83ed014",
+  ]
+}
---
+++ infrastructure/main.tf
@@ -0,0 +1,255 @@
+terraform {
+  backend "gcs" {
+    prefix = "terraform/state"
+  }
+}
+
+provider "google" {
+  project = var.google_cloud_project_id
+  region  = var.google_cloud_region
+  zone    = var.google_cloud_zone
+}
+
+data "google_project" "project" {
+}
+
+resource "google_pubsub_schema" "email-message" {
+  name       = "email-message"
+  type       = "AVRO"
+  definition = file("${path.module}/../schemas/src/main/resources/avro/email-message.avsc")
+}
+
+resource "google_pubsub_topic" "raw-emails" {
+  name = "raw-emails"
+
+  depends_on = [google_pubsub_schema.email-message]
+  schema_settings {
+    schema   = "${data.google_project.project.id}/schemas/${google_pubsub_schema.email-message.name}"
+    encoding = "BINARY"
+  }
+  message_retention_duration = "${31*24*60*60}s"
+  message_storage_policy {
+    allowed_persistence_regions = [var.google_cloud_region]
+  }
+}
+
+resource "google_pubsub_topic" "validated-emails" {
+  name = "validated-emails"
+
+  depends_on = [google_pubsub_schema.email-message]
+  schema_settings {
+    schema   = "${data.google_project.project.id}/schemas/${google_pubsub_schema.email-message.name}"
+    encoding = "BINARY"
+  }
+  message_retention_duration = "${31*24*60*60}s"
+  message_storage_policy {
+    allowed_persistence_regions = [var.google_cloud_region]
+  }
+}
+
+resource "google_pubsub_subscription" "raw-emails-to-bigquery" {
+  name  = "raw-emails-to-bigquery"
+  topic = google_pubsub_topic.raw-emails.name
+
+  bigquery_config {
+    table            = "${google_bigquery_table.raw-emails.project}.${google_bigquery_table.raw-emails.dataset_id}.${google_bigquery_table.raw-emails.table_id}"
+    use_topic_schema = true
+    write_metadata   = true
+  }
+
+  depends_on = [google_project_iam_member.viewer, google_project_iam_member.editor]
+}
+
+resource "google_pubsub_subscription" "validated-emails-to-bigquery" {
+  name  = "validated-emails-to-bigquery"
+  topic = google_pubsub_topic.validated-emails.name
+
+  bigquery_config {
+    table            = "${google_bigquery_table.validated-emails.project}.${google_bigquery_table.validated-emails.dataset_id}.${google_bigquery_table.validated-emails.table_id}"
+    use_topic_schema = true
+    write_metadata   = true
+  }
+
+  depends_on = [google_project_iam_member.viewer, google_project_iam_member.editor]
+}
+
+resource "google_project_iam_member" "viewer" {
+  project = data.google_project.project.project_id
+  role    = "roles/bigquery.metadataViewer"
+  member  = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-pubsub.iam.gserviceaccount.com"
+}
+
+resource "google_project_iam_member" "editor" {
+  project = data.google_project.project.project_id
+  role    = "roles/bigquery.dataEditor"
+  member  = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-pubsub.iam.gserviceaccount.com"
+}
+
+resource "google_bigquery_dataset" "pubsub" {
+  dataset_id = "pubsub"
+  location   = "EU"
+}
+
+resource "google_bigquery_table" "raw-emails" {
+  deletion_protection = false
+  table_id            = "raw-emails"
+  dataset_id          = google_bigquery_dataset.pubsub.dataset_id
+
+  schema = file("${path.module}/bigquery-emails-schema.json")
+}
+
+resource "google_bigquery_table" "validated-emails" {
+  deletion_protection = false
+  table_id            = "validated-emails"
+  dataset_id          = google_bigquery_dataset.pubsub.dataset_id
+
+  schema = file("${path.module}/bigquery-emails-schema.json")
+}
+
+resource "google_artifact_registry_repository" "images" {
+  format        = "docker"
+  repository_id = "images"
+}
+
+resource "google_compute_address" "smtp-to-pubsub" {
+  name = "smtp-to-pubsub"
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "google_compute_network" "k8s-main-cluster-network" {
+  name                    = "k8s-${var.k8s_cluster_name}-cluster-network"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "k8s-main-cluster-subnetwork" {
+  name               = "k8s-${var.k8s_cluster_name}-cluster-subnetwork"
+  network            = google_compute_network.k8s-main-cluster-network.name
+  ip_cidr_range      = "10.132.0.0/20"
+  secondary_ip_range = [
+    {
+      range_name    = "k8s-${var.k8s_cluster_name}-cluster-pod-range"
+      ip_cidr_range = "172.16.0.0/16"
+    },
+    {
+      range_name    = "k8s-${var.k8s_cluster_name}-cluster-service-range"
+      ip_cidr_range = "192.168.0.0/20"
+    }
+  ]
+}
+
+module "kubernetes-engine_safer-cluster" {
+  source  = "terraform-google-modules/kubernetes-engine/google//modules/safer-cluster"
+  version = "26.1.1"
+
+  name       = var.k8s_cluster_name
+  project_id = var.google_cloud_project_id
+  region     = var.google_cloud_region
+
+  network           = google_compute_network.k8s-main-cluster-network.name
+  subnetwork        = google_compute_subnetwork.k8s-main-cluster-subnetwork.name
+  ip_range_pods     = "k8s-${var.k8s_cluster_name}-cluster-pod-range"
+  ip_range_services = "k8s-${var.k8s_cluster_name}-cluster-service-range"
+
+  enable_private_endpoint = false
+
+  node_pools = [
+    {
+      name         = "${var.k8s_cluster_name}-node-pool"
+      machine_type = "e2-medium"
+      preemptible  = true
+    }
+  ]
+}
+
+module "kubernetes-engine_auth" {
+  source  = "terraform-google-modules/kubernetes-engine/google//modules/auth"
+  version = "26.1.1"
+
+  project_id           = var.google_cloud_project_id
+  cluster_name         = module.kubernetes-engine_safer-cluster.name
+  location             = module.kubernetes-engine_safer-cluster.location
+  use_private_endpoint = false
+}
+
+resource "google_service_account" "k8s-default-workload-account" {
+  account_id = "k8s-default-workload-account"
+}
+
+resource "google_project_iam_member" "k8s-pubsub-access" {
+  project = var.google_cloud_project_id
+  role    = "roles/pubsub.publisher"
+  member  = "serviceAccount:${google_service_account.k8s-default-workload-account.email}"
+}
+
+resource "google_service_account_iam_binding" "k8s-workload-account-binding" {
+  service_account_id = google_service_account.k8s-default-workload-account.name
+  role               = "roles/iam.workloadIdentityUser"
+  members            = ["serviceAccount:${var.google_cloud_project_id}.svc.id.goog[default/default]"]
+}
+
+resource "google_cloud_run_v2_service" "email-validator" {
+  name     = "email-validator"
+  location = var.google_cloud_region
+  ingress  = "INGRESS_TRAFFIC_INTERNAL_ONLY"
+
+  template {
+    containers {
+      image = "europe-west1-docker.pkg.dev/${var.google_cloud_project_id}/images/email-validator:${var.github_sha}"
+      env {
+        name  = "GOOGLE_CLOUD_PROJECT_NAME"
+        value = var.google_cloud_project_id
+      }
+    }
+  }
+}
+
+resource "google_pubsub_subscription" "email-validator" {
+  name  = "email-validator"
+  topic = google_pubsub_topic.raw-emails.name
+  push_config {
+    push_endpoint = google_cloud_run_v2_service.email-validator.uri
+    oidc_token {
+      service_account_email = "${data.google_project.project.number}-compute@developer.gserviceaccount.com"
+    }
+    attributes = {
+      x-goog-version = "v1"
+    }
+  }
+  retry_policy {
+    minimum_backoff = "10s"
+    maximum_backoff = "600s"
+  }
+}
+
+resource "google_pubsub_subscription" "my-page-api" {
+  name  = "my-page-api"
+  topic = google_pubsub_topic.validated-emails.name
+
+  ack_deadline_seconds = 600
+
+  push_config {
+    push_endpoint = var.api_jobposting_endpoint
+    oidc_token {
+      service_account_email = "${data.google_project.project.number}-compute@developer.gserviceaccount.com"
+    }
+    attributes = {
+      x-goog-version = "v1"
+    }
+  }
+  retry_policy {
+    minimum_backoff = "60s"
+    maximum_backoff = "600s"
+  }
+}
+
+resource "google_service_account" "cloud-sql-proxy" {
+  account_id = "cloud-sql-proxy"
+}
+
+resource "google_project_iam_member" "cloud-sql-proxy-client" {
+  project = data.google_project.project.project_id
+  role    = "roles/cloudsql.client"
+  member  = "serviceAccount:${google_service_account.cloud-sql-proxy.email}"
+}
