356ec21fb402c55cfe16843637cdd19de0c89abd
Petter <pettermailyahoo.com>
2024-09-17 11:25:42 +0200

    css +4

--- my-page-app/src/components/hyttebooking/month-overview/components/month-calendar/calendar-cell/CalendarCell.tsx
+++ my-page-app/src/components/hyttebooking/month-overview/components/month-calendar/calendar-cell/CalendarCell.tsx
@@ -3,6 +3,11 @@ import React from "react";
 import {CalendarDay} from "react-day-picker";
 import classes from "./CalendarCell.module.css";
 import BookingBar from "./booking-bar/BookingBar";
+import {
+    getIsDayOfWeek
+} from "@/components/hyttebooking/month-overview/components/month-calendar/calendar-date/calendarDateUtil";
+import {dateFormat} from "@/components/hyttebooking/month-overview/monthOverviewUtils";
+import { format } from 'date-fns';
 
 type Props = {
     bookings?: Booking[];
@@ -10,19 +15,43 @@ type Props = {
     user: User;
 }
 
-const CalendarCell = ({ bookings, day, user }: Props)  => {
+const CalendarCell = ({ bookings = [], day, user }: Props)  => {
     const style = classes;
-    const isPast = day.date < new Date();
+    const currentDate = new Date();
+    currentDate.setTime(currentDate.getTime() - ((24*60*60*1000) * 1));
+    const isPast =   day.date < currentDate;
+    const isWednesday = getIsDayOfWeek(day) === 3;
+    const dayString = format(day.date, dateFormat);
+    const hasPeriodEnd = (bookings || []).find(booking => booking?.endDate === dayString);
+    const hasPeriodStart = (bookings || []).find(booking => booking?.startDate === dayString);
+    const showAddButton = !isPast && isWednesday && !hasPeriodStart;
+    const showAddButtonPlaceholder = !isPast && isWednesday && !hasPeriodEnd && !showAddButton;
 
-    return (
+     return (
         <div className={`
-                ${style.calendarCell}
-                ${!isPast && style.calendarCellUpcoming}
-                ${isPast && style.calendarCellPast}
-            `}>
-            {bookings?.map(booking =>  (
-                <BookingBar day={day} user={user} booking={booking} />
+            ${style.calendarCell}
+            ${isWednesday && style.calendarCellWedDay}
+            ${!isPast && style.calendarCellUpcoming}
+            ${isPast && style.calendarCellPast}
+        `}>
+
+            {showAddButtonPlaceholder && (
+                <div className={style.addButtonContainer}/>
+            )}
+
+            {bookings?.map(booking => (
+                <BookingBar
+                    day={day}
+                    user={user}
+                    booking={booking}
+                />
             ))}
+
+            {showAddButton && (
+                <div className={style.addButtonContainer}>
+                    <button className={style.addButton}>+</button>
+                </div>
+            )}
         </div>
     );
 }
