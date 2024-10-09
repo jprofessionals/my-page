d313503364c45870c49ad23485cda1d5b6234e9d
Fredrik Haugseth <fredrik@jpro.no>
2024-10-09 13:13:27 +0200

    Generate job posting code from OpenAPI specification, and mock example response

---
+++ my-page-api/src/main/kotlin/no/jpro/mypageapi/controller/JobPostingController.kt
@@ -0,0 +1,36 @@
+package no.jpro.mypageapi.controller
+
+import no.jpro.mypageapi.api.JobPostingApiDelegate
+import no.jpro.mypageapi.model.JobPostingResponse
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Service
+import java.time.OffsetDateTime
+import java.time.ZoneOffset
+
+@Service
+class JobPostingController : JobPostingApiDelegate {
+
+    override fun getJobPostings(): ResponseEntity<List<JobPostingResponse>> {
+        val dto = listOf(
+            JobPostingResponse(
+                "Software Engineer",
+                "NAV",
+                OffsetDateTime.of(2024, 10, 9, 12, 0, 0, 0, ZoneOffset.UTC),
+                "Vi søker 5 dyktige utviklere med kompetanse i Kotlin og React",
+                listOf("Kotlin", "React"),
+                "1"
+            ),
+            JobPostingResponse(
+                "Frontend Developer",
+                "Skatteetaten",
+                OffsetDateTime.of(2024, 10, 15, 12, 0, 0, 0, ZoneOffset.UTC),
+                "Skatteetaten er på jakt etter en dyktig frontend-utvikler med kompetanse i Angular, TypeScript, og Azure",
+                listOf("Angular", "TypeScript", "Azure"),
+                "2"
+            )
+        )
+
+        return ResponseEntity.ok(dto)
+    }
+
+}
