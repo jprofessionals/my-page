4d91c5d543d761d6c2e38498cbd49673e26129c2
Fredrik Haugseth <fredrik@jpro.no>
2024-11-19 11:32:52 +0100

    Starting point for supporting App Router

---
+++ my-page-app/src/app/layout.tsx
@@ -0,0 +1,50 @@
+import type { Metadata } from 'next'
+import localFont from 'next/font/local'
+import './globals.scss'
+import Providers from '@/app/providers'
+import NavBar from '@/components/navbar/NavBar'
+import { ToastContainer } from 'react-toastify'
+
+const geistSans = localFont({
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900',
+})
+const geistMono = localFont({
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900',
+})
+
+export const metadata: Metadata = {
+  title: 'Min side',
+  description: 'Informasjon og verktøykasse for ansatte i JPro',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ToastContainer
+          position="top-right"
+          autoClose={2000}
+          hideProgressBar={false}
+          closeOnClick={true}
+          pauseOnHover={true}
+          draggable={true}
+          theme="colored"
+        />
+        <Providers>
+          <NavBar />
+          {children}
+        </Providers>
+      </body>
+    </html>
+  )
+}
