217af591cfdbbec66a640d58b26f1d1b61c4086e
Bram Bezem <bram@jpro.no>
2024-11-28 13:10:36 +0100

    fix: use icon

--- my-page-app/src/components/hyttebooking/month-overview/components/month-calendar/calendar-date/CalendarDate.tsx
+++ my-page-app/src/components/hyttebooking/month-overview/components/month-calendar/calendar-date/CalendarDate.tsx
@@ -3,6 +3,8 @@ import { CalendarDay } from 'react-day-picker'
 import { getIsDayOfWeek, getIsToday } from './calendarDateUtil'
 import classes from './CalendarDate.module.css'
 import { InfoBooking } from '../../../../../../types'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCircleExclamation} from "@fortawesome/free-solid-svg-icons";
 
 type Props = {
   day: CalendarDay
@@ -14,7 +16,7 @@ const CalendarDate = ({ day, infoNotices }: Props) => {
   const isToday = getIsToday(day)
   const isWednesday = getIsDayOfWeek(day) === 3
   const [showNoticeDescription, setShowNoticeDescription] =
-    useState<boolean>(!false)
+    useState<boolean>(false)
 
   return (
     <div className={style.container}>
@@ -24,7 +26,7 @@ const CalendarDate = ({ day, infoNotices }: Props) => {
             className={style.info}
             onClick={() => setShowNoticeDescription(!showNoticeDescription)}
           >
-            !
+            <FontAwesomeIcon icon={faCircleExclamation} className={style.icon} />
             {showNoticeDescription && (
               <div id="dialog" className={style.dialog}>
                 {infoNotice.description}
