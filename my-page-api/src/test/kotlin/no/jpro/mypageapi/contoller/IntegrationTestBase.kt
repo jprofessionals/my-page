60246fb20f8e0470f16715400b2120cafb56f8bc
tidalMatt <matthew.koranda@tidal.com>
2025-01-13 12:01:05 +0100

    Drop problematic dependency and retool tests

--- my-page-api/src/test/kotlin/no/jpro/mypageapi/contoller/IntegrationTestBase.kt
+++ my-page-api/src/test/kotlin/no/jpro/mypageapi/contoller/IntegrationTestBase.kt
@@ -1,20 +1,21 @@
 package no.jpro.mypageapi.contoller
 
 
-import no.jpro.mypageapi.config.TestRestTemplateConfiguration
 import no.jpro.mypageapi.repository.UserRepository
+import no.jpro.mypageapi.testutil.HttpHeaderTestRestTemplate
 import no.jpro.mypageapi.testutil.TestUserService
-import no.nav.security.token.support.spring.test.EnableMockOAuth2Server
+import no.nav.security.mock.oauth2.MockOAuth2Server
+import no.nav.security.mock.oauth2.token.DefaultOAuth2TokenCallback
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.context.annotation.Import
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpHeaders
 import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
-@Import(TestRestTemplateConfiguration::class)
-@EnableMockOAuth2Server(port = 8099)
 abstract class IntegrationTestBase (){
 
     @Autowired
@@ -23,10 +24,39 @@ abstract class IntegrationTestBase (){
     @Autowired
     private lateinit var userRepository: UserRepository
 
+    @Autowired
+    private lateinit var testRestTemplate: TestRestTemplate
+
+    var mockOAuth2Server = MockOAuth2Server()
+
+
     @BeforeEach
     fun setup() {
+        mockOAuth2Server = MockOAuth2Server()
+        mockOAuth2Server.start(8099)
+
         userRepository.deleteAll()
         userService.adminUser("test@test.no", 12345)
     }
 
+    @AfterEach
+    fun shutdown() {mockOAuth2Server.shutdown()}
+
+    fun restClient(authenticated: Boolean): HttpHeaderTestRestTemplate {
+        val client = HttpHeaderTestRestTemplate(testRestTemplate)
+        if (authenticated)
+            client.add(HttpHeaders.AUTHORIZATION) { generateBearerToken() }
+
+        return client
+    }
+
+    fun generateBearerToken(): String {
+        val token =
+            mockOAuth2Server.issueToken(
+                "default",
+                "test",
+                DefaultOAuth2TokenCallback(subject = "12345"),
+            )
+        return "Bearer " + token.serialize()
+    }
 }
--- my-page-api/src/test/kotlin/no/jpro/mypageapi/contoller/UserContollerTest.kt
+++ my-page-api/src/test/kotlin/no/jpro/mypageapi/contoller/UserContollerTest.kt
@@ -2,13 +2,11 @@ package no.jpro.mypageapi.contoller
 
 
 import no.jpro.mypageapi.dto.UserDTO
-import no.jpro.mypageapi.testutil.HttpHeaderTestRestTemplate
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 
-class UserContollerTest(@Autowired val httpHeaderTestRestTemplate: HttpHeaderTestRestTemplate)  : IntegrationTestBase() {
+class UserContollerTest()  : IntegrationTestBase() {
 
     @Test
     fun contextLoads() {}
@@ -16,8 +14,14 @@ class UserContollerTest(@Autowired val httpHeaderTestRestTemplate: HttpHeaderTes
 
     @Test
     fun fetchUser() {
-        val response = httpHeaderTestRestTemplate.getForEntity<List<UserDTO>>("/user")
+        val response = restClient(true).getForEntity<List<UserDTO>>("/user")
         assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
+    }
+
+    @Test
+    fun fetchUserAgain() {
+        val response = restClient(false).getForEntity<List<UserDTO>>("/user")
+        assertThat(response.statusCode).isEqualTo(HttpStatus.UNAUTHORIZED)
     }
 
 }
