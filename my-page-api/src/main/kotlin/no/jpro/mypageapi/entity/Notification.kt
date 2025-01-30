c0d6ec423920cd300a62a3a38798e92433af8108
tidalMatt <matthew.koranda@tidal.com>
2025-01-30 13:09:54 +0100

    1651 - Add Notification table and entity

---
+++ my-page-api/src/main/kotlin/no/jpro/mypageapi/entity/Notification.kt
@@ -0,0 +1,14 @@
+package no.jpro.mypageapi.entity
+
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "notification", uniqueConstraints = [UniqueConstraint(columnNames = ["user_id", "job_posting_id"])])
+data class Notification(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+    val userId: Long,
+    var jobPostingId: Long
+)
