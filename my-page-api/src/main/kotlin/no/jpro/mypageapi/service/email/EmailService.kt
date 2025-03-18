eb4a53c5d69a9ea338a367a5c2ea7f1d3acb2989
tidalMatt <matthew.koranda@tidal.com>
2025-03-18 15:25:44 +0100

    Not multipart

--- my-page-api/src/main/kotlin/no/jpro/mypageapi/service/email/EmailService.kt
+++ my-page-api/src/main/kotlin/no/jpro/mypageapi/service/email/EmailService.kt
@@ -4,6 +4,7 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.mail.SimpleMailMessage
 import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.mail.javamail.MimeMessageHelper
 import org.springframework.stereotype.Service
 
 
@@ -15,11 +16,13 @@ class EmailService (
     private val logger = LoggerFactory.getLogger(EmailService::class.java.name)
 
     fun sendSimpleMessage(to: String, subject: String, text: String) {
-        val message = SimpleMailMessage()
-        message.from = sender
-        message.setTo(to)
-        message.subject = subject
-        message.text = text
+        val message = emailSender.createMimeMessage()
+        val helper = MimeMessageHelper(message)
+
+        helper.setFrom(sender)
+        helper.setTo(to)
+        helper.setSubject(subject)
+        helper.setText(text)
         logger.info("Sending message $message")
 
         emailSender.send(message)
